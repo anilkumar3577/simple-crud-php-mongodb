9f4fc8b2023f4fe95baafd10c7e41fb59f143a90
anilkumar3577 <107928396+anilkumar3577@users.noreply.github.com>
2023-03-14 20:48:56 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,23 +1,24 @@
 <?php
 /**
-* Connection to MongoDB
-  
+ * Connection to MongoDB
+ * 
  * $connection = new MongoClient(); // connects to localhost:27017
-  
+ * 
  * For remote host connection
-  
+ * 
  * $connection = new MongoClient( "mongodb://example.com" ); // connect to a remote host (default port: 27017)
  * $connection = new MongoClient( "mongodb://example.com:65432" ); // connect to a remote host at a given port
-  
-  * Connection using database username and password
+ * 
+ * Connection using database username and password
+ * 
+ * $connectionUrl = sprintf('mongodb://%s:%d/%s', $host, $port, $database);
+ * $connection = new Mongo($connectionUrl, array('username' => $username, 'password' => $password));
  */ 
-
-$connection = new Mongo($connectionUrl, array('username' => $username, 'password' => $password));
-  
 $connection = new MongoClient();
 
 /**
-* Select database named "test"
+ * Select database named "test"
  */ 
 $db = $connection->test;
 ?>
+
